c4d0c6aa962793e549ca8312d29de854e0b1f3ed
asakoh <hirro0326@gmail.com>
2020-07-05 10:53:19 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -3,15 +3,40 @@
 int n;
 int k;
 int A[100000];
+int p(int x) {
+    int i, sum = 0;
+    for (i = 0; i < n; i++) {
+        sum = sum + A[i] / x;
+    }
+    return k > sum; //‘„‚ªì‚ê‚È‚­‚È‚é‚Æ1‚ğ•Ô‚·
+}
 
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
+int main() {
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+    }
 
+    int max = 0;
+    for (i = 0; i < n; i++) {
+        if (A[i] > max) {
+            max = A[i];
+        }
+    }
+    lb = 0;
+    ub = max;
 
-  return 0;
+    while (ub - lb > 1) {
+        int mid = (lb + ub) / 2;
+        if (p(mid)) {
+            ub = mid;
+        }
+        else {
+            lb = mid;
+        }
+    }
+    printf("%d\n", lb);
+    return 0;
 }
